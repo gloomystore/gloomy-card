dcdf579396ef80d017b3796a368c42399792d14b
gloomystore <pikchu3333@gmail.com>
2024-07-21 20:26:55 +0900

    feat: 프리즘 배경 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
 export default function Home() {
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const container = e.currentTarget as HTMLDivElement;
+    const overlay = e.currentTarget.querySelector('.overlay') as HTMLDivElement;
+    // console.log(overlay)
+    if (!container || !overlay) return;
+
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    overlay.style.backgroundPosition = `${x / 5 + y / 5}px ${x / 5 + y / 5}px`;
+    
+    const rotateX = 4 / 30 * y - 20;
+    const rotateY = -1 / 5 * x + 20;
+    container.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  };
+
+  const handleMouseOut = (e: React.MouseEvent<HTMLDivElement>) => {
+    const container = e.currentTarget as HTMLDivElement;
+    const overlay = e.currentTarget.querySelector('.overlay') as HTMLDivElement;
+
+    if (!container || !overlay) return;
+
+    // overlay.style.filter = 'opacity(0)';
+    overlay.style.backgroundPosition = `100%`
+    container.style.transform = `perspective(350px) rotateX(0deg) rotateY(0deg)`;
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="aura">
+        <div className="aura-content">
+          {[...Array(5)].map((_, index) => (
+            <div 
+              key={index} 
+              className="container" 
+              onMouseMove={handleMouseMove} 
+              onMouseOut={handleMouseOut}
+            >
+              <div className="overlay"></div>
+              <div className="card"></div>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
